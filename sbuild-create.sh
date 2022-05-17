3c624f3236106266cac61a1a8f43fce87eda048b
Luis Guzmán <ark@switnet.org>
2022-05-17 04:55:20 +0000

    Add debian support for os and jail.

--- sbuild-create.sh
+++ sbuild-create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #    Copyright (C) 2018-2021  Ruben Rodriguez <ruben@trisquel.info>
+#    Copyright (C) 2022  Luis Guzman <ark@switnet.org>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,87 +19,158 @@
 
 #https://wiki.debian.org/sbuild
 
-if [ $EUID -ne 0 ]; then
+# True if $1 is greater than $2
+version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+if [ "$EUID" -ne 0 ]; then
    echo "This script must be run as root"
    exit 1
 fi
 
 if [ $# -lt 2 ] && [ $# -gt 3 ]; then
-   echo Usage: $0 CODENAME ARCH UPSTREAM\(optional\)
-   echo Example: $0 nabia amd64 upstream
+   echo Usage: "$0" CODENAME ARCH UPSTREAM \(optional\)
+   echo Example 1: "$0" nabia amd64
+   echo Example 2: "$0" bullseye amd64
+   echo Example 3: "$0" aramo amd64 upstream
    exit 1
 fi
 
 set -e
 
-CODENAME=$1
-ARCH=$2
-UPSTREAM=$3
+CODENAME="$1"
+ARCH="$2"
+UPSTREAM="$3"
 
-[ "$ARCH" = "i386" ] || [ "$ARCH" = "armhf" ] && BITS=32
+[ "$ARCH" = "i386"  ] || [ "$ARCH" = "armhf" ] && BITS=32
 [ "$ARCH" = "amd64" ] || [ "$ARCH" = "arm64" ] && BITS=64
 PORTS=false
 [ "$ARCH" = "armhf" ] || [ "$ARCH" = "arm64" ] && PORTS=true
 
+[ "$CODENAME" == "buster"   ] && UPSTREAM="debian" && VALID=1
+[ "$CODENAME" == "bullseye" ] && UPSTREAM="debian" && VALID=1
+[ "$CODENAME" == "bookworm" ] && UPSTREAM="debian" && VALID=1
+[ "$CODENAME" == "sid"      ] && UPSTREAM="debian" && VALID=1
+
 UBUSRC=http://archive.ubuntu.com/ubuntu
-$PORTS && UBUSRC=http://ports.ubuntu.com/
+"$PORTS" && UBUSRC=http://ports.ubuntu.com/
 
 if [ "$UPSTREAM" = "upstream" ];then
 REPO=http://archive.ubuntu.com/ubuntu
-$PORTS && REPO=$UBUSRC
+"$PORTS" && REPO="$UBUSRC"
+elif [ "$UPSTREAM" = "debian" ]; then
+REPO=http://deb.debian.org/debian
 else
 REPO=http://archive.trisquel.org/trisquel
 fi
 
-[ "$CODENAME" == "aramo" ] && UBURELEASE=jammy
-[ "$CODENAME" == "nabia" ] && UBURELEASE=focal
-[ "$CODENAME" == "etiona" ] && UBURELEASE=bionic
-[ "$CODENAME" == "flidas" ] && UBURELEASE=xenial
-[ "$CODENAME" == "belenos" ] && UBURELEASE=trusty
+[ "$CODENAME" == "aramo"  ] && UBURELEASE=jammy  && VALID=1
+[ "$CODENAME" == "nabia"  ] && UBURELEASE=focal  && VALID=1
+[ "$CODENAME" == "etiona" ] && UBURELEASE=bionic && VALID=1
+
+if [ "$VALID" != 1 ];then
+    echo "Not valid codename"
+    exit 1
+fi
+
+HOST_OS="$(lsb_release -si)"
+DBSTRAP_VER="$(apt-cache madison debootstrap | sort -r | awk 'NR==1{print$3}')"
+echo -e "\\nOS: $HOST_OS \\ndebootstrap: $DBSTRAP_VER\\n"
+
+#Upgrade debian debootstrap package if required.
+if [ "$HOST_OS" = Debian ] && \
+   version_gt 1.0.124 "$DBSTRAP_VER" && \
+   [ "$CODENAME" = aramo ];then
+    echo "It is required to upgrade debootstrap to create a $CODENAME jail, upgrading..."
+cat << REPO >> /etc/apt/sources.list
+
+#Pinned repo (see /etc/apt/preferences.d/debootstrap).
+deb http://deb.debian.org/debian bullseye-backports main
+REPO
+    apt update -q2
+cat << DBST > /etc/apt/preferences.d/debootstrap
+Package: *
+Pin: release n=bullseye-backports
+Pin-Priority: 1
+DBST
+    apt-get -t bullseye-backports install debootstrap
+fi
 
 CA_BASE="$CODENAME-$ARCH"
 SBUILD_CREATE_DIR="/tmp/sbuild-create/$CA_BASE"
 
 if [ "$UPSTREAM" = "upstream" ];then
-CODENAME=$UBURELEASE
+CODENAME="$UBURELEASE"
 UNIVERSE="universe"
 fi
 
-umount $SBUILD_CREATE_DIR/proc || true
-umount $SBUILD_CREATE_DIR/ || true
-rm -rf $SBUILD_CREATE_DIR
-mkdir -p $SBUILD_CREATE_DIR
-mount -t tmpfs none $SBUILD_CREATE_DIR
+umount "$SBUILD_CREATE_DIR"/proc || true
+umount "$SBUILD_CREATE_DIR"/ || true
+rm -rf "$SBUILD_CREATE_DIR"
+mkdir -p "$SBUILD_CREATE_DIR"
+mount -t tmpfs none "$SBUILD_CREATE_DIR"
+DBSTRAP_SCRIPTS="/usr/share/debootstrap/scripts"
 
-if [ "$UPSTREAM" = "upstream" ];then
-    TMP_KEY=`mktemp -d`
-    LATEST_LTS_KEYRING="http://archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-keyring/ubuntu-keyring_2021.03.26.tar.gz"
-    KEYRING_PATH="ubuntu-keyring-2021.03.26/keyrings"
-    KEYRING_FILE="ubuntu-archive-keyring.gpg"
-    DBSTRAP_SCRIPTS="/usr/share/debootstrap/scripts"
-    curl -s -4  $LATEST_LTS_KEYRING | tar xvfz  - $KEYRING_PATH/$KEYRING_FILE
-    mv $KEYRING_PATH/$KEYRING_FILE $TMP_KEY/$KEYRING_FILE
-    rm -r $(dirname $KEYRING_PATH)
-    PRE_BUILD_KEYRING="--keyring=$TMP_KEY/$KEYRING_FILE"
-    if [ ! -f $DBSTRAP_SCRIPTS/$CODENAME ];then
-        if [ -f  $DBSTRAP_SCRIPTS/trisquel ]; then
-            ln -s $DBSTRAP_SCRIPTS/trisquel $DBSTRAP_SCRIPTS/$CODENAME
-        elif [ -f $DBSTRAP_SCRIPTS/gutsy ]; then
-            ln -s $DBSTRAP_SCRIPTS/gutsy $DBSTRAP_SCRIPTS/$CODENAME
-        else
-           echo "No option available"
-           exit
+if [ "$HOST_OS" = "Trisquel" ];then
+    if [ "$UPSTREAM" = "upstream" ];then
+        TMP_KEY="$(mktemp -d)"
+        LATEST_LTS_KEYRING="http://archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-keyring/ubuntu-keyring_2021.03.26.tar.gz"
+        KEYRING_PATH="ubuntu-keyring-2021.03.26/keyrings"
+        KEYRING_FILE="ubuntu-archive-keyring.gpg"
+        curl -s -4  "$LATEST_LTS_KEYRING" | tar xvfz  - "$KEYRING_PATH"/"$KEYRING_FILE"
+        mv "$KEYRING_PATH"/"$KEYRING_FILE" "$TMP_KEY"/"$KEYRING_FILE"
+        rm -r "$(dirname $KEYRING_PATH)"
+        PRE_BUILD_KEYRING="--keyring=$TMP_KEY/$KEYRING_FILE"
+        if [ ! -f "$DBSTRAP_SCRIPTS"/"$CODENAME" ];then
+            if [ -f  "$DBSTRAP_SCRIPTS"/trisquel ]; then
+                ln -s "$DBSTRAP_SCRIPTS"/trisquel "$DBSTRAP_SCRIPTS"/"$CODENAME"
+            elif [ -f "$DBSTRAP_SCRIPTS"/gutsy ]; then
+                ln -s "$DBSTRAP_SCRIPTS"/gutsy "$DBSTRAP_SCRIPTS"/"$CODENAME"
+            else
+               echo "No option available"
+               exit
+            fi
         fi
     fi
 fi
-
-debootstrap --arch=$ARCH --variant=minbase --components=main $PRE_BUILD_KEYRING --include=apt $CODENAME $SBUILD_CREATE_DIR $REPO
-
-if [ "$UPSTREAM" != "upstream" ];then
-wget http://builds.trisquel.org/repos/signkey.asc  -O $SBUILD_CREATE_DIR/tmp/key.asc
+if [ "$HOST_OS" != "Trisquel" ];then
+    if [ -z "$UPSTREAM" ];then
+        TMP_TKEY="$(mktemp -d)"
+        TRISQUEL_ARCHIVE_KEYRING="http://archive.trisquel.org/trisquel/trisquel-archive-signkey.gpg"
+        curl -s -4 "$TRISQUEL_ARCHIVE_KEYRING" > "$TMP_TKEY"/trisquel-archive-signkey
+        gpg --dearmor "$TMP_TKEY"/trisquel-archive-signkey
+        PRE_BUILD_KEYRING="--keyring=$TMP_TKEY/trisquel-archive-signkey.gpg"
+        DBSTAP_TRISQUEL="https://gitlab.trisquel.org/trisquel/package-helpers/-/raw/nabia/helpers/DATA/debootstrap/trisquel"
+        DBSTRAP_TRIS_COM="https://gitlab.trisquel.org/trisquel/package-helpers/-/raw/nabia/helpers/DATA/debootstrap/trisquel-common"
+        [ ! -f "$DBSTRAP_SCRIPTS"/"$CODENAME"     ] && curl -s -4 "$DBSTAP_TRISQUEL" > "$DBSTRAP_SCRIPTS"/"$CODENAME"
+        [ ! -f "$DBSTRAP_SCRIPTS"/trisquel-common ] && curl -s -4 "$DBSTRAP_TRIS_COM" > "$DBSTRAP_SCRIPTS"/trisquel-common
+    elif [ "$UPSTREAM" = upstream ];then
+        apt install -y ubuntu-archive-keyring
+        PRE_BUILD_KEYRING="--keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg"
+        [ ! -f "$DBSTRAP_SCRIPTS"/"$CODENAME" ] && ln -s "$DBSTRAP_SCRIPTS"/gutsy "$DBSTRAP_SCRIPTS"/"$CODENAME"
+    fi
+fi
+if [ "$UPSTREAM" = "debian" ];then
+    apt install -y debian-archive-keyring
+    PRE_BUILD_KEYRING="--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
+    DBSTAP_SID="https://salsa.debian.org/installer-team/debootstrap/-/raw/master/scripts/sid"
+    DBSTRAP_DEB_COM="https://salsa.debian.org/installer-team/debootstrap/-/raw/master/scripts/debian-common"
+    [ ! -f "$DBSTRAP_SCRIPTS"/"$CODENAME"   ] && curl -s -4 "$DBSTAP_SID" > "$DBSTRAP_SCRIPTS"/"$CODENAME"
+    [ ! -f "$DBSTRAP_SCRIPTS"/debian-common ] && curl -s -4 "$DBSTRAP_DEB_COM" > "$DBSTRAP_SCRIPTS"/debian-common
 fi
 
-cat << MAINEOF > $SBUILD_CREATE_DIR/finish.sh
+debootstrap --arch="$ARCH" \
+            --variant=minbase \
+            --components=main \
+            "$PRE_BUILD_KEYRING" \
+            --include=apt \
+            "$CODENAME" \
+            "$SBUILD_CREATE_DIR" "$REPO"
+
+if [ -z "$UPSTREAM" ];then
+wget http://builds.trisquel.org/repos/signkey.asc  -O "$SBUILD_CREATE_DIR"/tmp/key.asc
+fi
+
+cat << MAINEOF > "$SBUILD_CREATE_DIR"/finish.sh
 #!/bin/bash
 set -x
 set -e
@@ -119,6 +191,14 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 33C66596
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0C05112F
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com FED8FD3E
+#Debian
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 9D6D8F6BC857C906
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 8B48AD6246925553
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com DCC9EFBF77E11517
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 648ACFD622F3D138
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 54404762BBB6E853
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 605C66F00D6C9793
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0E98404D386FA1D9
 }
 
 echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/force-unsafe-io
@@ -128,7 +208,7 @@ echo "no-use-agent" > /home/jenkins/.gnupg/gpg.conf
 chown 1007.1008 -R /home/jenkins
 
 #Add keys to trisquel schroot
-if [ "$UPSTREAM" != "upstream" ]; then
+if [ -z "$UPSTREAM" ]; then
 add_sbuild_keys
 cat /tmp/key.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/builds-repo-key.gpg  >/dev/null
 fi
@@ -139,11 +219,11 @@ echo set debconf/frontend Noninteractive | debconf-communicate
 echo set debconf/priority critical | debconf-communicate
 # Install basic build tool set, trying to match buildd
 apt-get -y --force-yes install build-essential
-apt-get -y --force-yes install --no-install-recommends fakeroot apt-utils pkgbinarymangler apt devscripts zip unzip quilt wget lsb-release gnupg
-apt-get -y --force-yes install --no-install-recommends aptitude || true
+apt-get -y --force-yes install --no-install-recommends fakeroot apt-utils apt devscripts zip unzip quilt wget lsb-release gnupg
+apt-get -y --force-yes install --no-install-recommends aptitude pkgbinarymangler || true
 
 #Add keys to upstream schroot (first get universe requirements).
-if [ "$UPSTREAM" = "upstream" ]; then
+if [ "$UPSTREAM" = "upstream" ] && [ "$UPSTREAM" = "debian" ]; then
 add_sbuild_keys
 fi
 
@@ -162,7 +242,7 @@ rm /finish.sh
 echo Finished self-setup
 MAINEOF
 
-cat << EOF > $SBUILD_CREATE_DIR/etc/apt/sources.list
+cat << EOF > "$SBUILD_CREATE_DIR"/etc/apt/sources.list
 deb $REPO $CODENAME main $UNIVERSE
 deb $REPO $CODENAME-updates main $UNIVERSE
 deb $REPO $CODENAME-security main $UNIVERSE
@@ -173,7 +253,7 @@ deb-src $REPO $CODENAME-security main $UNIVERSE
 
 EOF
 if [ "$UPSTREAM" != "upstream" ];then
-cat << EOF >> $SBUILD_CREATE_DIR/etc/apt/sources.list
+cat << EOF >> "$SBUILD_CREATE_DIR"/etc/apt/sources.list
 #Trisquel builds repositories
 deb http://builds.trisquel.org/repos/$CODENAME/ $CODENAME main
 deb http://builds.trisquel.org/repos/$CODENAME/ $CODENAME-security main
@@ -185,18 +265,51 @@ deb-src $UBUSRC $UBURELEASE-security main universe
 
 EOF
 fi
+if [ "$UPSTREAM" = "debian" ];then
+    if [ "$CODENAME" = "sid"      ]; then
+cat << EOF > "$SBUILD_CREATE_DIR"/etc/apt/sources.list
+deb $REPO $CODENAME main
+deb-src $REPO $CODENAME main
 
-mount -o bind /proc $SBUILD_CREATE_DIR/proc
-chroot $SBUILD_CREATE_DIR bash -x /finish.sh
-umount $SBUILD_CREATE_DIR/proc
+EOF
+    fi
+    if [ "$CODENAME" = "bullseye" ] || \
+       [ "$CODENAME" = "bookworm" ]; then
+cat << EOF > "$SBUILD_CREATE_DIR"/etc/apt/sources.list
+deb $REPO $CODENAME main
+deb $REPO $CODENAME-updates main
+deb $REPO-security $CODENAME-security main
 
-rm -rf /var/lib/schroot/chroots/$CA_BASE
+deb-src $REPO $CODENAME main
+deb-src $REPO $CODENAME-updates main
+deb-src $REPO-security $CODENAME-security main
+
+EOF
+    fi
+    if [ "$CODENAME" = "buster" ]; then
+cat << EOF > "$SBUILD_CREATE_DIR"/etc/apt/sources.list
+deb $REPO $CODENAME main
+deb $REPO $CODENAME-updates main
+deb http://security.debian.org/debian-security $CODENAME/updates main
+
+deb-src $REPO $CODENAME main
+deb-src $REPO $CODENAME-updates main
+deb-src http://security.debian.org/debian-security $CODENAME/updates main
+
+EOF
+    fi
+fi
+mount -o bind /proc "$SBUILD_CREATE_DIR"/proc
+chroot "$SBUILD_CREATE_DIR" bash -x /finish.sh
+umount "$SBUILD_CREATE_DIR"/proc
+
+rm -rf /var/lib/schroot/chroots/"$CA_BASE"
 [ -d /var/lib/schroot/chroots ] || mkdir /var/lib/schroot/chroots
-cp -a $SBUILD_CREATE_DIR /var/lib/schroot/chroots/$CA_BASE
-umount $SBUILD_CREATE_DIR
-rm -r $SBUILD_CREATE_DIR
+cp -a "$SBUILD_CREATE_DIR" /var/lib/schroot/chroots/"$CA_BASE"
+umount "$SBUILD_CREATE_DIR"
+rm -r "$SBUILD_CREATE_DIR"
 
-cat << EOF > /etc/schroot/chroot.d/sbuild-$CA_BASE
+cat << EOF > /etc/schroot/chroot.d/sbuild-"$CA_BASE"
 [$CA_BASE]
 description=$CODENAME-$ARCH $UPSTREAM build.
 groups=sbuild,root
@@ -235,11 +348,12 @@ chmod a+rx /etc/schroot/setup.d/04tmpfs
 
 fi
 
-if [ "$UPSTREAM" = "upstream" ];then
+if [ "$UPSTREAM" = "upstream" ] || [ "$UPSTREAM" = "debian" ];then
     [ -f /usr/share/debootstrap/scripts/$CODENAME ] && rm /usr/share/debootstrap/scripts/$CODENAME
+    [ -f /usr/share/debootstrap/scripts/debian-common ] && rm /usr/share/debootstrap/scripts/debian-common
 fi
 
-sbuild-update -udcar $CA_BASE
+sbuild-update -udcar "$CA_BASE"
 
 echo "Setup of schroot $CODENAME-$ARCH finished successfully"
 
